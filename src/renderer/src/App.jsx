33da910beb9db72f7da69fc71164e81db3678664
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-21 16:24:42 +0200

    Removed the App.jsx

--- src/renderer/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default () => {
-    return (
-        <>
-            <h1 style={{textAlign: "center"}}>Dualboot Manager</h1>
-            <p style={{textAlign: "center"}}>Work in progress...</p>
-        </>
-    )
-};
