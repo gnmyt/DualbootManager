fd840e0853ccff34b3e9692b952b474427952a8f
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-21 16:25:10 +0200

    Updated the main.jsx

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import "@common/styles/main.sass";
+import {createHashRouter, RouterProvider,} from "react-router-dom";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import Root from "@common/layouts/Root";
+
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <Root/>
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router}/>
     </React.StrictMode>
 );
