55f59877b89d61e334205cea73c6c95526b1796f
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-21 15:40:27 +0200

    Created the preload index.js

---
+++ src/preload/index.js
@@ -0,0 +1,16 @@
+import { contextBridge } from "electron";
+import { electronAPI } from "@electron-toolkit/preload";
+
+const api = {};
+
+if (process.contextIsolated) {
+  try {
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', api)
+  } catch (error) {
+    console.error(error);
+  }
+} else {
+  window.electron = electronAPI;
+  window.api = api;
+}
