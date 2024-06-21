b03aec7bb07a1faa80193470f6389b42db11ae5f
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-21 15:41:25 +0200

    Created the App.jsx

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,8 @@
+export default () => {
+    return (
+        <>
+            <h1 style={{textAlign: "center"}}>Dualboot Manager</h1>
+            <p style={{textAlign: "center"}}>Work in progress...</p>
+        </>
+    )
+};
