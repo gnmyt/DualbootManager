2a7ca3ced2b503cf3b59938e7542c389879155a8
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-22 12:14:28 +0200

    Created the Navigation component

---
+++ src/renderer/src/common/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import "./styles.sass";
+
+export const Navigation = () => {
+    return (
+        <div className="navigation">
+            <h1 className="nav-active">theming</h1>
+            <h1>settings</h1>
+            <h1>update</h1>
+        </div>
+    )
+}
