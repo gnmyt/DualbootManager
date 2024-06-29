a046b857d8bbc9d67e5f972485917679faf8d1ac
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-29 20:45:50 +0200

    Added contextual page switching in the ConfigurationContext.jsx

--- src/renderer/src/common/contexts/ConfigurationContext.jsx
+++ src/renderer/src/common/contexts/ConfigurationContext.jsx
@@ -1,12 +1,28 @@
-import {createContext} from "react";
-import {Navigate} from "react-router-dom";
+import {createContext, useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export const ConfigurationContext = createContext([]);
 
 export const ConfigurationProvider = ({children}) => {
+
+    const [mountPoint, setMountPoint] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        window.electron.ipcRenderer.invoke("request-bootloader-mount")
+            .then((data) => {
+                if (data === null) {
+                    navigate("/setup/uac-prompt");
+                    return;
+                }
+
+                setMountPoint(data);
+                navigate("/home/themes");
+            });
+    }, []);
+
     return (
-        <ConfigurationContext.Provider value={[]}>
-            <Navigate to={"/setup/uac-prompt"}/>
+        <ConfigurationContext.Provider value={mountPoint}>
             {children}
         </ConfigurationContext.Provider>
     );
