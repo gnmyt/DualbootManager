07cff89166c6723292a2f6669a6f720dd7f725ec
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-29 20:45:56 +0200

    Updated the Finished.jsx

--- src/renderer/src/pages/setup/Finished/Finished.jsx
+++ src/renderer/src/pages/setup/Finished/Finished.jsx
@@ -3,16 +3,21 @@ import {faChevronRight, faRefresh} from "@fortawesome/free-solid-svg-icons";
 import Button from "@common/components/Button";
 import {useNavigate} from "react-router-dom";
 import BIOSImage from "./assets/bios.png";
-
 export const Finished = () => {
+    const navigate = useNavigate();
+
+    const rebootNow = () => {
+        window.electron.ipcRenderer.send("reboot-to-bios");
+    }
+
     return (
         <div className="space-inner finished-page">
             <div className="info-area">
                 <h1>Finished!</h1>
                 <p>You now may want to change your boot order to boot CLOVER first (if not applied automatically).</p>
                 <div className="button-area">
-                    <Button icon={faChevronRight} text="Reboot later"></Button>
-                    <Button icon={faRefresh} text="Reboot now"></Button>
+                    <Button icon={faChevronRight} text="Reboot later" onClick={() => navigate("/home/themes")}></Button>
+                    <Button icon={faRefresh} text="Reboot now" onClick={rebootNow}></Button>
 
                 </div>
             </div>
