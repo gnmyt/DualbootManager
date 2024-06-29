54f5b21bab22903fee25a4e9a60cdea997e13002
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-29 20:45:24 +0200

    Implemented a proper navigation

--- src/renderer/src/common/components/Navigation/Navigation.jsx
+++ src/renderer/src/common/components/Navigation/Navigation.jsx
@@ -1,11 +1,20 @@
+import {useLocation, useNavigate} from "react-router-dom";
 import "./styles.sass";
 
 export const Navigation = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const isActive = (path) => {
+        return location.pathname === `/home/${path}`;
+    }
+
     return (
         <div className="navigation">
-            <h1 className="nav-active">theming</h1>
-            <h1>settings</h1>
-            <h1>update</h1>
+            <h1 className={isActive("themes") ? "nav-active" : ""} onClick={() => navigate("/home/themes")}>theming</h1>
+            <h1 className={isActive("settings") ? "nav-active" : ""} onClick={() => navigate("/home/settings")}>settings</h1>
+            <h1 className={isActive("update") ? "nav-active" : ""} onClick={() => navigate("/home/update")}>update</h1>
         </div>
     )
 }
