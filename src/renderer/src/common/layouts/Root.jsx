34d94512eccd28f9679f7126845b1b161fad58a0
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-30 15:31:12 +0200

    Fixed the Root.jsx layout

--- src/renderer/src/common/layouts/Root.jsx
+++ src/renderer/src/common/layouts/Root.jsx
@@ -1,34 +1,34 @@
 import {useLocation, useOutlet} from "react-router-dom";
 import {ConfigurationProvider} from "@common/contexts/ConfigurationContext";
-import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import {CSSTransition, SwitchTransition} from 'react-transition-group'
 import {routes} from "../../main";
+import {useRef} from "react";
 
 export default () => {
     const location = useLocation();
     const currentOutlet = useOutlet();
 
     const route = routes.find(route => route.path === location.pathname);
-    const nodeRef = route?.nodeRef;
+    const nodeRef = route?.nodeRef || useRef(null);
 
     return (
         <div className="root">
-            <ConfigurationProvider/>
-            <div className="blur"></div>
-            <SwitchTransition>
-                <CSSTransition
-                    key={location.pathname}
-                    nodeRef={nodeRef}
-                    timeout={500}
-                    classNames="page"
-                    unmountOnExit
-                >
-                    {(state) => (
+            <ConfigurationProvider>
+                <div className="blur"></div>
+                <SwitchTransition>
+                    <CSSTransition
+                        key={location.pathname}
+                        nodeRef={nodeRef}
+                        timeout={500}
+                        classNames="page"
+                        unmountOnExit
+                    >
                         <div ref={nodeRef} className="page">
                             {currentOutlet}
                         </div>
-                    )}
-                </CSSTransition>
-            </SwitchTransition>
+                    </CSSTransition>
+                </SwitchTransition>
+            </ConfigurationProvider>
         </div>
     );
 }
