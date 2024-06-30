f442087bd1d04568dad95e2c68c8ff4adc7df0aa
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-30 15:31:52 +0200

    Implemented proper theme management in the Themes.jsx

--- src/renderer/src/pages/home/Themes/Themes.jsx
+++ src/renderer/src/pages/home/Themes/Themes.jsx
@@ -1,20 +1,30 @@
 import "./styles.sass";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import Navigation from "@common/components/Navigation";
+import {ConfigurationContext} from "@common/contexts/ConfigurationContext";
+import SmallButton from "@common/components/SmallButton";
+import {faUpload} from "@fortawesome/free-solid-svg-icons";
 
 export const Themes = () => {
     const [themes, setThemes] = useState([]);
+    const {configuration, updateConfiguration} = useContext(ConfigurationContext);
 
     useEffect(() => {
         window.electron.ipcRenderer.invoke("request-themes").then(setThemes);
     }, []);
+
+    const installTheme = async (theme) => {
+        await window.electron.ipcRenderer.invoke("copy-theme", theme);
+
+        updateConfiguration("GUI.Theme", theme);
+    }
 
     return (
         <div className="themes-page">
             <Navigation />
             <div className="themes-area">
                 {themes.map((theme, index) => (
-                    <div key={index} className="theme">
+                    <div key={index} className={"theme" + (configuration?.GUI?.Theme === theme.name ? " theme-active" : "")}>
                         <img src={"data:image/png;base64," + theme.thumbnail} alt="Screenshot" />
                         <div className="theme-inner">
                             <div className="theme-title">
@@ -22,6 +32,10 @@ export const Themes = () => {
                                 <p>by {theme.author}</p>
                             </div>
                             <p className="theme-description">{theme.description}</p>
+                        </div>
+                        <div className="theme-actions">
+                            <SmallButton onClick={() => installTheme(theme.name)} icon={faUpload} text="Install"
+                                         disabled={configuration?.GUI?.Theme === theme.name} />
                         </div>
                     </div>
                 ))}
