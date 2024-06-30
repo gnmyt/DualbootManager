ce86e8cb176d5823536770099c71351c6cf825c2
Mathias Wagner <germannewsmaker@gmail.com>
2024-06-30 15:30:07 +0200

    Implemented transactions in the config.js util

--- src/main/util/config.js
+++ src/main/util/config.js
@@ -1,5 +1,9 @@
 import plist from "plist";
-import {retrieveEFIEntries} from "./partition";
+import {retrieveBootloaderMount, retrieveEFIEntries} from "./partition";
+import sudo from "sudo-prompt";
+import {INSTALLATION_PATH} from "../index";
+import fs from "node:fs";
+import path from "node:path";
 
 const DEFAULT_CONFIG = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -34,6 +38,8 @@ const DEFAULT_CONFIG = `<?xml version="1.0" encoding="UTF-8"?>
                 <key>Speed</key>
                 <integer>3</integer>
             </dict>
+            <key>Theme</key>
+            <string>embedded</string>
             <key>Language</key>
             <string>en</string>
             <key>ScreenResolution</key>
@@ -71,6 +77,142 @@ export const getDefaultConfig = async () => {
 
 }
 
-export const loadConfig = async () => {
+export const startTransaction = async () => {
+    const bootloaderMount = await retrieveBootloaderMount();
+    const mountDir = path.join(INSTALLATION_PATH, 'efi_tmp');
+    if (!fs.existsSync(mountDir)) fs.mkdirSync(mountDir);
 
+    const transactionDir = path.join(INSTALLATION_PATH, 'transaction');
+    if (!fs.existsSync(transactionDir)) fs.mkdirSync(transactionDir);
+
+    const bashCommand = `
+        existing_mount_point=$(findmnt -rn -o TARGET ${bootloaderMount})
+        if [ -z "$existing_mount_point" ]; then
+            mount ${bootloaderMount} ${mountDir}
+            mount_point=${mountDir}
+        else
+            mount_point=$existing_mount_point
+        fi
+        if [ ! -d $mount_point/EFI ]; then exit 1; fi
+        cp -r $mount_point/EFI/CLOVER/themes ${transactionDir}
+        cp -r $mount_point/EFI/CLOVER/config.plist ${transactionDir}
+        chmod -R 777 ${transactionDir}
+        `;
+
+    fs.writeFileSync(path.join(INSTALLATION_PATH, 'start_transaction.sh'), bashCommand);
+
+    return new Promise((resolve, reject) => {
+        sudo.exec(`bash ${path.join(INSTALLATION_PATH, 'start_transaction.sh')}`, { name: 'Clover Bootloader Installer' }, (error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+export const commitTransaction = async () => {
+    const bootloaderMount = await retrieveBootloaderMount();
+    const mountDir = path.join(INSTALLATION_PATH, 'efi_tmp');
+    if (!fs.existsSync(mountDir)) fs.mkdirSync(mountDir);
+
+    const transactionDir = path.join(INSTALLATION_PATH, 'transaction');
+
+    const bashCommand = `
+        existing_mount_point=$(findmnt -rn -o TARGET ${bootloaderMount})
+        if [ -z "$existing_mount_point" ]; then
+            mount ${bootloaderMount} ${mountDir}
+            mount_point=${mountDir}
+        else
+            mount_point=$existing_mount_point
+        fi
+        if [ ! -d $mount_point/EFI ]; then exit 1; fi
+        rm -rf $mount_point/EFI/CLOVER/themes
+        rm -f $mount_point/EFI/CLOVER/config.plist
+        cp -r ${transactionDir}/themes $mount_point/EFI/CLOVER
+        cp -r ${transactionDir}/config.plist $mount_point/EFI/CLOVER
+        `;
+
+    fs.writeFileSync(path.join(INSTALLATION_PATH, 'commit_transaction.sh'), bashCommand);
+
+    return new Promise((resolve, reject) => {
+        sudo.exec(`bash ${path.join(INSTALLATION_PATH, 'commit_transaction.sh')}`, { name: 'Clover Bootloader Installer' }, (error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+export const getConfig = async () => {
+    const configPath = path.join(INSTALLATION_PATH, 'transaction', 'config.plist');
+
+    return new Promise((resolve, reject) => {
+        fs.readFile(configPath, 'utf8', (error, data) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve( plist.parse(data) );
+        });
+    });
+}
+
+export const updateConfig = async (config) => {
+    const configPath = path.join(INSTALLATION_PATH, 'transaction', 'config.plist');
+
+    return new Promise((resolve, reject) => {
+        fs.writeFile(configPath, plist.build(config), (error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+export const clearThemes = async () => {
+    const transactionDir = path.join(INSTALLATION_PATH, 'transaction');
+    const themesDir = path.join(transactionDir, 'themes');
+
+    const defaultThemes = ['embedded', 'random'];
+
+    return new Promise((resolve, reject) => {
+        fs.readdir(themesDir, (error, files) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            files.forEach(file => {
+                if (!defaultThemes.includes(file)) {
+                    fs.rmSync(path.join(themesDir, file), { recursive: true });
+                }
+            });
+            resolve();
+        });
+    });
+
+}
+
+export const copyTheme = async (theme) => {
+    const transactionDir = path.join(INSTALLATION_PATH, 'transaction');
+    const themesDir = path.join(INSTALLATION_PATH, 'themes');
+
+    await clearThemes();
+
+    const themeDir = path.join(themesDir, theme);
+
+    return new Promise((resolve, reject) => {
+        fs.cp(themeDir, path.join(transactionDir, 'themes', theme), {recursive: true}, (error) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve();
+        });
+    });
 }
